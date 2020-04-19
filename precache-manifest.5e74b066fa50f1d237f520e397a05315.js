dd2ee407ffdaedec448b1928b8f80fbcca8515fc
prajwalbharadwaj <prajwalrb17@gmail.com>
2020-04-19 20:04:54 +0530

    adding git pages

---
+++ precache-manifest.5e74b066fa50f1d237f520e397a05315.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c96403f53a0a9a6331",
+    "url": "/designportfolio/css/chunk-vendors.cff9d0e4.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/designportfolio/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/designportfolio/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5bcbea13b655515af5d58c95a2716274",
+    "url": "/designportfolio/index.html"
+  },
+  {
+    "revision": "059b9f308cfee2847006",
+    "url": "/designportfolio/js/about.240316ab.js"
+  },
+  {
+    "revision": "4e1223d54dd598e50b37",
+    "url": "/designportfolio/js/app.e69fe8b0.js"
+  },
+  {
+    "revision": "d0c96403f53a0a9a6331",
+    "url": "/designportfolio/js/chunk-vendors.f51f874f.js"
+  },
+  {
+    "revision": "3dae0eb28ce912ce546cc08f80ddd314",
+    "url": "/designportfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/designportfolio/robots.txt"
+  }
+]);
